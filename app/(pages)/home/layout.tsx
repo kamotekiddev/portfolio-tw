20d0cbdcc39845ec578bc8f37aa0d5b012171125
kamote-kid <kamote-kid@gmail.com>
2023-07-14 13:46:02 +0800

    create layout for new home page

---
+++ app/(pages)/home/layout.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+interface Props {
+   children: ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
+   return (
+      <main className="min-h-screen max-w-4xl mx-auto">
+         <Header />
+         <div className="h-full">{children}</div>
+         <Footer />
+      </main>
+   );
+};
+
+export default Layout;
