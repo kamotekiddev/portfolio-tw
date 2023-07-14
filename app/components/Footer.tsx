e621866a54c733238ee4469db05b48d81c6b80aa
kamote-kid <kamote-kid@gmail.com>
2023-07-14 21:51:17 +0800

    build the footer component

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
+import Logo from "./Logo";
+import IconButton from "./IconButton";
 
 const Footer = () => {
-   return <div>Footer</div>;
+   return (
+      <div className="p-4 flex justify-between items-center bg-indigo-50">
+         <Logo />
+         <p>Copyright © 2022 Joshua Dela Cruz - All rights reserved.</p>
+         <div className="space-x-2">
+            <IconButton icon={FaGithub} />
+            <IconButton icon={FaFacebook} />
+            <IconButton icon={FaLinkedin} />
+         </div>
+      </div>
+   );
 };
 
 export default Footer;
