aed6c599a8b4f5bddece194ecaeff427ad53706b
kamote-kid <kamote-kid@gmail.com>
2023-07-14 09:50:22 +0800

    extract a logo component

---
+++ app/components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () => {
+   return (
+      <h1 className="text-2xl font-black">
+         KK<span className="text-indigo-600">D</span>
+      </h1>
+   );
+};
+
+export default Logo;
