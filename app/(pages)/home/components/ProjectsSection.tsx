537df1fd62a621917ff7f71e1c18469d6fab45ee
kamote-kid <kamote-kid@gmail.com>
2023-07-15 17:45:01 +0800

    responsive project section

--- app/(pages)/home/components/ProjectsSection.tsx
+++ app/(pages)/home/components/ProjectsSection.tsx
@@ -6,7 +6,7 @@ const ProjectsSection = () => {
          <h1 className="mb-8 w-max rounded-lg bg-indigo-50 p-2 px-4 text-3xl font-black text-indigo-600 dark:bg-indigo-600 dark:text-white">
             Recent Projects
          </h1>
-         <div className="grid grid-cols-2 gap-4">
+         <div className="grid gap-4 md:grid-cols-2">
             <ProjectCard
                title="Kamote Chat"
                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quam natus quasi dolorem nesciunt? Commodi possimus quam earum nulla ab?"
