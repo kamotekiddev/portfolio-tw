a7881e6d54226eda7b82644b81ffbe7b74475487
kamote-kid <kamote-kid@gmail.com>
2023-08-14 20:09:54 +0800

    add the contact animation to the container

--- app/(pages)/components/ContactSection/index.tsx
+++ app/(pages)/components/ContactSection/index.tsx
@@ -8,34 +8,36 @@ const ContactSection = () => {
       <section className="mx-auto max-w-7xl px-4 py-20">
          <div className="mb-10">
             <SectionHeader>Contacts</SectionHeader>
-            <p className="text-justify text-paragraph-1 leading-loose">
+            <motion.p
+               initial={{ opacity: 0 }}
+               whileInView={{ opacity: 1 }}
+               transition={{ type: "spring", delay: 0.5 }}
+               className="text-justify text-paragraph-1 leading-loose"
+            >
                Are you in search of a talented and experienced Frontend
                Developer to elevate your digital presence? Look no further! I am
                here to bring my skills and expertise to the table and help you
                achieve your business goals. Just contact me right here.
-            </p>
+            </motion.p>
          </div>
-         <div className="space-y-10 rounded-md bg-white-primary p-10  dark:bg-black-secondary">
-            <motion.div
-               initial={{ opacity: 0 }}
-               whileInView={{ opacity: 1 }}
-               transition={{ delay: 0.5, duration: 0.5 }}
-            >
+         <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ type: "spring", delay: 0.5 }}
+            className="space-y-10 rounded-md bg-white-primary p-10  dark:bg-black-secondary"
+         >
+            <div>
                <h2 className="mb-4 text-heading-5 font-black">Email Address</h2>
                <div className="space-y-4">
                   <CopyableItem label="kamotekid.dev@gmail.com" />
                   <CopyableItem label="delacruzjoshua691@gmail.com" />
                </div>
-            </motion.div>
-            <motion.div
-               initial={{ opacity: 0 }}
-               whileInView={{ opacity: 1 }}
-               transition={{ delay: 0.5, duration: 0.5 }}
-            >
+            </div>
+            <div>
                <h2 className="mb-4 text-heading-5 font-black">Contact No.</h2>
                <CopyableItem label="+63 966 2048 118" />
-            </motion.div>
-         </div>
+            </div>
+         </motion.div>
       </section>
    );
 };
