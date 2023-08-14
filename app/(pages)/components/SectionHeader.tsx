38b7102a71c28eeb1acf548de4a3bac72983baf3
kamote-kid <kamote-kid@gmail.com>
2023-08-14 20:23:06 +0800

    add animtaion to the section header

--- app/(pages)/components/SectionHeader.tsx
+++ app/(pages)/components/SectionHeader.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { motion } from "framer-motion";
 import { ReactNode } from "react";
 
 interface Props {
@@ -6,9 +7,14 @@ interface Props {
 }
 const SectionHeader = ({ children: chidlren }: Props) => {
    return (
-      <h1 className="mb-10 inline-block rounded-lg text-heading-1 font-black leading-none text-accent-orange">
+      <motion.h1
+         initial={{ opacity: 0 }}
+         whileInView={{ opacity: 1 }}
+         transition={{ type: "spring", delay: 0.5 }}
+         className="mb-10 inline-block rounded-lg text-heading-1 font-black leading-none text-accent-orange"
+      >
          {chidlren}
-      </h1>
+      </motion.h1>
    );
 };
 
