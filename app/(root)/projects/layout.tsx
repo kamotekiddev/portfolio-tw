55e5cc8779f3654cb8faff65f576cbf8bd5faf12
kamote-kid <kamote-kid@gmail.com>
2023-08-15 22:18:21 +0800

    add social links and darkmode toggle on projects page

--- app/(root)/projects/layout.tsx
+++ app/(root)/projects/layout.tsx
@@ -3,20 +3,31 @@ import React, { ReactNode } from "react";
 import Button from "@/components/Button";
 import Footer from "@/components/Footer";
 import Link from "next/link";
+import DarkModeToggle from "@/components/DarkModeToggle";
+import socialLinks from "@/data/social-links";
+import IconButton from "@/components/IconButton";
 
 interface Props {
    children: ReactNode;
 }
 const Layout = ({ children }: Props) => {
    return (
-      <main>
-         <header className="sticky top-0 z-[1000] bg-black-primary">
-            <div className="mx-auto max-w-7xl p-4">
+      <main className="bg-hero-bg-white dark:bg-hero-bg-dark">
+         <header className="sticky top-0 z-[1000] backdrop-blur-lg">
+            <div className="mx-auto flex max-w-7xl justify-between p-4">
                <Link href="/">
                   <Button variant="outline" size="medium">
                      Go back
                   </Button>
                </Link>
+               <div className="space-x-2">
+                  {socialLinks.map(({ icon, link }, i) => (
+                     <a href={link} target="_black" key={i}>
+                        <IconButton icon={icon} />
+                     </a>
+                  ))}
+                  <DarkModeToggle />
+               </div>
             </div>
          </header>
          {children}
