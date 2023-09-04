a5bb2957af8c5896b677bb89ba37ce96170f4bf2
kamote-kid <kamote-kid@gmail.com>
2023-09-04 10:30:30 +0800

    remove unused assets

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -14,9 +14,9 @@ const projects: Project[] = [
          "/images/loop-studios-asset/4.png",
       ],
       "github-link":
-         "https://github.com/ND-Ice/frontend-mentor.loopstudios-landing-page.github.io",
+         "https://github.com/kamotekiddev/frontend-mentor.loopstudios-landing-page.github.io",
       "live-link":
-         "https://nd-ice.github.io/frontend-mentor.loopstudios-landing-page.github.io",
+         "https://kamotekiddev.github.io/frontend-mentor.loopstudios-landing-page.github.io",
    },
    {
       id: 2,
@@ -32,8 +32,10 @@ const projects: Project[] = [
          "/images/chirp-asset/3.png",
          "/images/chirp-asset/4.png",
       ],
-      "github-link": "https://github.com/ND-Ice/codewell.cc.chirp-landing-page",
-      "live-link": "https://nd-ice.github.io/codewell.cc.chirp-landing-page",
+      "github-link":
+         "https://github.com/kamotekiddev/codewell.cc.chirp-landing-page",
+      "live-link":
+         "https://kamotekiddev.github.io/codewell.cc.chirp-landing-page",
    },
    {
       id: 3,
@@ -51,9 +53,9 @@ const projects: Project[] = [
          "/images/blogr-asset/5.png",
       ],
       "github-link":
-         "https://github.com/ND-Ice/nd-ice-front-end-mentor.blogr-landing-page.github.io",
+         "https://github.com/kamotekiddev/nd-ice-front-end-mentor.blogr-landing-page.github.io",
       "live-link":
-         "https://nd-ice.github.io/nd-ice-front-end-mentor.blogr-landing-page.github.io",
+         "https://kamotekiddev.github.io/nd-ice-front-end-mentor.blogr-landing-page.github.io",
    },
    {
       id: 4,
@@ -72,42 +74,9 @@ const projects: Project[] = [
          "/images/sunnyside-asset/6.png",
       ],
       "github-link":
-         "https://github.com/ND-Ice/nd-ice.sunnyside-agency-landing-page-front-end-mentor.github.io",
+         "https://github.com/kamotekiddev/nd-ice.sunnyside-agency-landing-page-front-end-mentor.github.io",
       "live-link":
-         "https://nd-ice.github.io/nd-ice.sunnyside-agency-landing-page-front-end-mentor.github.io",
-   },
-   {
-      id: 5,
-      projectTitle: "Tribubo",
-      projectDescription:
-         "An experimental web app that i built while practicing next js and Chakra UI that's inspired by the twitter UI.",
-      category: "Front-End",
-      toolsUsed: ["Next Js", "Chakra UI", "Next Auth", "Sanity"],
-      projectImageBanner: "/images/project-image/tribubo-banner.png",
-      projectImages: [
-         "/images/tribubo-asset/1.png",
-         "/images/tribubo-asset/3.png",
-         "/images/tribubo-asset/2.png",
-         "/images/tribubo-asset/4.png",
-         "/images/tribubo-asset/5.png",
-      ],
-      "github-link": "https://github.com/ND-Ice/trave-blog",
-   },
-   {
-      id: 6,
-      projectTitle: "Foodie",
-      projectDescription:
-         "A web-app project that will help a person to view and search a recipe that is according to their diet with complete instructions and nutrition facts using external API.",
-      category: "Front-End",
-      toolsUsed: ["React Js", "Styled-Components", "External API"],
-      projectImageBanner: "/images/project-image/foodie-banner.png",
-      projectImages: [
-         "/images/foodie-asset/1.png",
-         "/images/foodie-asset/3.png",
-         "/images/foodie-asset/2.png",
-         "/images/foodie-asset/4.png",
-      ],
-      "github-link": "https://github.com/ND-Ice/foodie",
+         "https://kamotekiddev.github.io/nd-ice.sunnyside-agency-landing-page-front-end-mentor.github.io",
    },
    {
       id: 7,
@@ -125,49 +94,6 @@ const projects: Project[] = [
       ],
       "github-link": "https://github.com/ND-Ice/movie-matters",
       "live-link": "https://movie-matters.vercel.app",
-   },
-   {
-      id: 8,
-      projectTitle: "Finder",
-      projectDescription:
-         "A web-app project and school requirement on Software Engineering Subject, the goal of this project is to help the new students find a suitable and affordable boarding houses for their stay.",
-      category: "FullStack",
-      toolsUsed: ["React Js", "CSS Modules", "Node Js", "Express", "Heroku"],
-      projectImageBanner: "/images/project-image/finder-banner.png",
-      projectImages: [
-         "/images/finder-asset/1.png",
-         "/images/finder-asset/3.png",
-         "/images/finder-asset/2.png",
-         "/images/finder-asset/4.png",
-      ],
-      "github-link": "https://github.com/ND-Ice/finder",
-   },
-
-   {
-      id: 9,
-      projectTitle: "E-IPCR",
-      projectDescription:
-         "A web-app project and school requirement and my Thesis, the goal of this project is to automate the process of the Individual Performance Commitment Review (IPCR) Evaluation on the College of Arts and Sciences.",
-      category: "FullStack",
-      toolsUsed: [
-         "React Js",
-         "Redux",
-         "Bootstrap",
-         "Styled-Components",
-         "Node Js",
-         "Express",
-         "Heroku",
-      ],
-      projectImageBanner: "/images/project-image/e-ipcr-banner.png",
-      projectImages: [
-         "/images/e-ipcr-asset/1.png",
-         "/images/e-ipcr-asset/3.png",
-         "/images/e-ipcr-asset/2.png",
-         "/images/e-ipcr-asset/4.png",
-         "/images/e-ipcr-asset/5.png",
-      ],
-      "github-link": "https://github.com/ND-Ice/e-ipcr-client",
-      "live-link": "https://e-ipcr-faculty.herokuapp.com",
    },
 ];
 
