e5563070d614b415fdcd3299a2425384db461ff2
kamote-kid <kamote-kid@gmail.com>
2023-09-26 21:35:28 +0800

    remove the react-scroll library

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,14 +1,8 @@
-"use client";
-
-import { Link } from "react-scroll";
-
 const Logo = () => {
    return (
-      <Link to="home" smooth className="cursor-pointer" offset={-100}>
-         <h1 className="inline-block bg-custom-gradient bg-clip-text text-heading-6 font-black text-transparent">
-            KAMOTE KID
-         </h1>
-      </Link>
+      <h1 className="inline-block bg-custom-gradient bg-clip-text text-heading-6 font-black text-transparent">
+         KAMOTE KID
+      </h1>
    );
 };
 
