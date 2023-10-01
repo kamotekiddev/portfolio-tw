d20a0284baecf4c5c0585b8de748f263b6bb4286
kamote-kid <kamote-kid@gmail.com>
2023-10-01 20:37:36 +0800

    add cursor spotlight on projects page

--- app/(root)/projects/layout.tsx
+++ app/(root)/projects/layout.tsx
@@ -1,4 +1,7 @@
-import React, { ReactNode } from "react";
+"use client";
+import { useTheme } from "next-themes";
+import React, { ReactNode, MouseEvent } from "react";
+import { useMotionValue, motion, useMotionTemplate } from "framer-motion";
 
 import Button from "@/components/Button";
 import Footer from "@/components/Footer";
@@ -11,8 +14,25 @@ interface Props {
    children: ReactNode;
 }
 const Layout = ({ children }: Props) => {
+   const { theme } = useTheme();
+   let mouseX = useMotionValue(0);
+   let mouseY = useMotionValue(0);
+
+   const handleMouseMove = ({
+      clientX,
+      clientY,
+      currentTarget,
+   }: MouseEvent) => {
+      const { left, top } = currentTarget.getBoundingClientRect();
+      mouseX.set(clientX - left);
+      mouseY.set(clientY - top);
+   };
+
    return (
-      <main className="bg-hero-bg-white dark:bg-hero-bg-dark">
+      <main
+         onMouseMove={handleMouseMove}
+         className="group/container relative bg-hero-bg-white dark:bg-hero-bg-dark"
+      >
          <header className="sticky top-0 z-[1000] backdrop-blur-lg">
             <div className="mx-auto flex max-w-5xl justify-between p-4">
                <Link href="/">
@@ -32,6 +52,26 @@ const Layout = ({ children }: Props) => {
          </header>
          {children}
          <Footer />
+         <motion.div
+            className="pointer-events-none absolute inset-0 z-[1000] rounded-xl opacity-0 transition duration-300 group-hover/container:opacity-100"
+            style={{
+               background:
+                  theme === "dark"
+                     ? useMotionTemplate`
+            radial-gradient(
+              650px circle at ${mouseX}px ${mouseY}px,
+              rgba(14, 165, 233, 0.15),
+              transparent 80%
+            )`
+                     : useMotionTemplate`
+          radial-gradient(
+            650px circle at ${mouseX}px ${mouseY}px,
+            rgba(4, 1, 4, 0.15),
+            transparent 80%
+          )
+        `,
+            }}
+         />
       </main>
    );
 };
