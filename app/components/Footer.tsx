e6c4a7599e7cfcaf1ef4f3cbcb8bcc4321483426
kamotekid <kamotekiddev@gmail.com>
2024-08-09 09:49:01 +0800

    refactor: remove social links on footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,4 @@
 import Logo from "./Logo";
-import IconButton from "./IconButton";
-import socialLinks from "@/data/social-links";
 
 const Footer = () => {
    return (
@@ -8,13 +6,6 @@ const Footer = () => {
          <div className="mx-auto flex max-w-5xl flex-col items-center justify-center gap-4 p-4 text-center md:flex-row md:justify-between">
             <Logo />
             <p>Copyright © 2022 Joshua Dela Cruz - All rights reserved.</p>
-            <div className="space-x-2">
-               {socialLinks.map(({ icon, link }, i) => (
-                  <a href={link} target="_black" key={i}>
-                     <IconButton icon={icon} />
-                  </a>
-               ))}
-            </div>
          </div>
       </div>
    );
